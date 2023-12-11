cb5394ce2996c6dfcac404c56cebf4af523969d9
Bonface Mwangi Wairimu <bwmwangi@safaricom.co.ke>
2023-12-11 13:00:04 +0300

    commit all

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    // https://mvnrepository.com/artifact/org.apache.spark/spark-core
-    implementation 'org.apache.spark:spark-core_2.13:3.5.0'
+
+
 
 
 }
