e156226737bf881be3bbc145549b317f64ba4bd7
Bonface Mwangi Wairimu <bwmwangi@safaricom.co.ke>
2023-12-13 10:23:49 +0300

    encode update

--- src/main/java/ke/co/safaricom/Encode.java
+++ src/main/java/ke/co/safaricom/Encode.java
@@ -12,4 +12,48 @@ public class Encode {
     public int getKey() {
         return this.key;
     }
+    public String getInputText() {
+        return this.message;
+    }
+
+    public static String encrypt(String message, int key) {
+        String encryptedMessage = " ";
+        String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        message = message.toUpperCase();
+        int charPosition;
+        int keyValue;
+        char encryptedValue;
+
+        for (int i = 0; i < message.length(); i++) {
+            switch (message.charAt(i)) {
+                case ' ': encryptedMessage += ' ';
+                    break;
+                case '.': encryptedMessage += '.';
+                    break;
+                case ',': encryptedMessage += ',';
+                    break;
+                case '!': encryptedMessage += '!';
+                    break;
+                case '?': encryptedMessage += '?';
+                    break;
+
+                default: charPosition = letters.indexOf(message.charAt(i));
+
+                    keyValue = (key + charPosition) % 26;
+
+                    if (keyValue <  0) {
+                        keyValue = letters.length() + keyValue;
+                    }
+
+                    encryptedValue = letters.charAt(keyValue);
+                    encryptedMessage += encryptedValue;
+                    break;
+            }
+        }
+
+        return encryptedMessage;
+
+    }
+
+}
 
